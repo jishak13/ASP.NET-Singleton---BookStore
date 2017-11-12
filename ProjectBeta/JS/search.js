4c081b07ac5d651917a968bfaf5a75e62678a9e0
Conro <conor.wroble@gmail.com>
2017-11-12 13:58:06 -0700

    My First Commit

---
+++ ProjectBeta/JS/search.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $("#ser-input").focus(function () {
+        $('.search-full-view').addClass("search-normal-screen");
+
+    });
+    $("#search-close").click(function () {
+        $('.search-full-view').removeClass("search-normal-screen");
+    
+    });
+});
