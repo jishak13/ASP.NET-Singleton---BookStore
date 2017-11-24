a849b60811e9a8c4225fb036e3c9dcdb579c0d37
jishak13 <jishak1@asu.edu>
2017-11-24 09:30:05 -0800

    Added comments to cart items and products.

--- BookStore2-dev-branch/BookStore2-dev-branch/ProjectBeta/CartItem.cs
+++ BookStore2-dev-branch/BookStore2-dev-branch/ProjectBeta/CartItem.cs
@@ -5,28 +5,56 @@ using System.Web;
 
 namespace ProjectBeta
 {
+    /// <summary>
+    /// Public class for Cart Item Objects
+    /// Uses IEquatable interface to compare different cart Items
+    /// </summary>
     public class CartItem: IEquatable<CartItem>
     {
+        /// <summary>
+        /// Private instance variables
+        /// </summary>
         Products product;
         int quantity;
-
+        /// <summary>
+        /// Public read and write Properties
+        /// </summary>
         public Products Product { get => product; set => product = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+        /// <summary>
+        /// Public read only variables
+        /// </summary>
         public string Title { get { return Product.Title; } }
         public string Author { get { return Product.Author; } }
-        public int Quantity { get => quantity; set => quantity = value; }
         public decimal UnitPrice { get { return Convert.ToDecimal(Product.Price); } }
         public decimal TotalPrice { get { return Convert.ToDecimal(Product.Price) * Quantity; } }
+
+        /// <summary>
+        /// Default Contructor
+        /// </summary>
         public CartItem() { }
+        /// <summary>
+        /// Contructor for Cart Item that creates a Cart Item.
+        /// </summary>
+        /// <param name="prod">The Product to be added to the Cart</param>
         public CartItem(Products prod)
         {
             Product = prod;
             
         }
-
+        /// <summary>
+        /// Override to string to print. Title       Author     PRice     Quantity
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{Product.Title}\t\t\t {Product.Author}\t\t {Product.Price}\t{Quantity}";
         }
+        /// <summary>
+        ///Method that tests whether two Cart Items are equal
+        /// </summary>
+        /// <param name="item">The Item that is being compared</param>
+        /// <returns>If the items are equal, return true. Else if they arent equal return false.</returns>
         public bool Equals(CartItem item)
         {
             return item.Product.Title == Product.Title;
--- BookStore2-dev-branch/BookStore2-dev-branch/ProjectBeta/Products.cs
+++ BookStore2-dev-branch/BookStore2-dev-branch/ProjectBeta/Products.cs
@@ -5,9 +5,17 @@ using System.Web;
 
 namespace ProjectBeta
 {
+    /// <summary>
+    /// Searlizable tag for setting products to a gridView
+    /// </summary>
     [Serializable]
+    
+    ///Public Class for PRoducts Objects
     public class Products
     {
+        /// <summary>
+        /// Private variables
+        /// </summary>
         private string isbn;
         private string author;
         private string title;
@@ -15,6 +23,15 @@ namespace ProjectBeta
         private string code;
         private string price;
 
+        /// <summary>
+        /// Constructor for a product taking in all variables
+        /// </summary>
+        /// <param name="isbn">The ISBN of the product.</param>
+        /// <param name="author">The Author of the product</param>
+        /// <param name="title">The Title of the product</param>
+        /// <param name="course">The  Course for this product</param>
+        /// <param name="code">The Code for this product</param>
+        /// <param name="price">The Price for this product</param>
         public Products(string isbn, string author, string title, string course, string code, string price)
         {
             this.Isbn = isbn;
@@ -25,13 +42,35 @@ namespace ProjectBeta
             this.Price = price;
         }
 
+        /// <summary>
+        /// Public property for the ISBN
+        /// </summary>
         public string Isbn { get => isbn; set => isbn = value; }
+        /// <summary>
+        /// Public property for the Author
+        /// </summary>
         public string Author { get => author; set => author = value; }
+        /// <summary>
+        /// Public property for the Title
+        /// </summary>
         public string Title { get => title; set => title = value; }
+        /// <summary>
+        /// Public property for the Course
+        /// </summary>
         public string Course { get => course; set => course = value; }
+        /// <summary>
+        /// Public property for the Code
+        /// </summary>
         public string Code { get => code; set => code = value; }
+        /// <summary>
+        /// Public property for the Price
+        /// </summary>
         public string Price { get => price; set => price = value; }
 
+        /// <summary>
+        /// Override the ToString to Print all the properties of the book.
+        /// </summary>
+        /// <returns>Returns the String output</returns>
         public override string ToString()
         {
             return $"{Title}\t{Author}\t{Isbn}\t{Course}\t{Code}\t{Price}<br>";
